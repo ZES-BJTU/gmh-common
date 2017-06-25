e7eb624d7b7c8cc3fd7ef28359d516e72689fb93
zhouyuhuan <1120726720@qq.com>
2017-06-25 23:46:36 +0800

    添加缓存信息

--- src/main/java/com/zes/squad/gmh/common/constant/RedisConsts.java
+++ src/main/java/com/zes/squad/gmh/common/constant/RedisConsts.java
@@ -2,4 +2,7 @@ package com.zes.squad.gmh.common.constant;
 
 public class RedisConsts {
 
+    public static final int TEN_MINUTE    = 60 * 10;
+    public static final int THIRTY_MINUTE = 60 * 30;
+
 }
