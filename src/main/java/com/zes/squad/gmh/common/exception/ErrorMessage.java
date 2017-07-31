7e8998c827d4c3a1e39395abd0c88b915be067fd
zhouyuhuan <1120726720@qq.com>
2017-07-31 15:31:24 +0800

    添加错误提示信息

---
+++ src/main/java/com/zes/squad/gmh/common/exception/ErrorMessage.java
@@ -0,0 +1,10 @@
+package com.zes.squad.gmh.common.exception;
+
+public class ErrorMessage {
+
+    public static String paramIsNull     = "参数为空";
+    public static String pageNumIsError  = "分页页码错误";
+    public static String pageSizeIsError = "分页大小错误";
+    public static String staffIsNull     = "用户为空";
+
+}
