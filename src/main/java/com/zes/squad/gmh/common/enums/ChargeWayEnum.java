d95e30a9cf8db6368a67beede8c2abd99c82c20e
zhouyuhuan <1120726720@qq.com>
2017-07-31 11:31:03 +0800

    添加枚举

--- src/main/java/com/zes/squad/gmh/common/enums/ChargeWayEnum.java
+++ src/main/java/com/zes/squad/gmh/common/enums/ChargeWayEnum.java
@@ -6,11 +6,10 @@ import lombok.Getter;
 
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
-public enum AppointmentStatusEnum {
+public enum ChargeWayEnum {
 
-    TO_DO(1, "待完成"),
-    DONE(2, "已完成"),
-    CANCEL(3, "已取消");
+    CARD(1, "会员卡"),
+    CASH(2, "现金");
 
     private int    key;
     private String desc;
