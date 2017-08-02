6e264d7e4a2b03afce7cd9ee8feca14c54866ef3
zhouyuhuan <1120726720@qq.com>
2017-08-02 14:27:39 +0800

    添加总页数计算

--- src/main/java/com/zes/squad/gmh/common/entity/PagedLists.java
+++ src/main/java/com/zes/squad/gmh/common/entity/PagedLists.java
@@ -12,13 +12,15 @@ public class PagedLists {
 
     public static <T> PagedList<T> newPagedList(int pageNum, int pageSize) {
         PagedList<T> pagedList = new PagedList<>();
-        pagedList.setPageNum(pageNum).setPageSize(pageSize).setTotalCount(0L).setData(Lists.newArrayList());
+        pagedList.setPageNum(pageNum).setPageSize(pageSize).setTotalCount(0L).setTotalPages(0)
+                .setData(Lists.newArrayList());
         return pagedList;
     }
 
     public static <T> PagedList<T> newPagedList(int pageNum, int pageSize, long totalCount, List<T> data) {
         PagedList<T> pagedList = new PagedList<>();
         pagedList.setPageNum(pageNum).setPageSize(pageSize).setTotalCount(totalCount)
+                .setTotalPages((int) Math.ceil(((double) totalCount) / pageSize))
                 .setData(data == null ? Lists.newArrayList() : data);
         return pagedList;
     }
